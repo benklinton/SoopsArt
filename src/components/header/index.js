4530a0e6bda8075914c55eb100c5ea56a0ba3fce
benklinton <benklinton@gmail.com>
2020-11-05 13:14:05 -0700

    changed header text

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,8 +8,8 @@ function Header() {
             <Container className=" h-100">
                 <div className="row h-100 align-items-center">
                     <div className="col-12 text-center">
-                        <h1 className="font-weight-light">Vertically Centered Masthead Content</h1>
-                        <p className="lead">A great starter layout for a landing page</p>
+                        <h1 className="font-weight-heavy"> Welcome to Soops Art</h1>
+                        <p className="lead">Art created by Sophie Gifford</p>
                     </div>
                 </div>
             </Container>
