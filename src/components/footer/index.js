42087597d4866616e12e05677af442cc521fde5a
benklinton <benklinton@gmail.com>
2020-11-05 12:14:05 -0700

    created footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return (
+        <footer className="py-4 bg-footer text-white-50">
+        <div className="container text-center">
+          <small>Copyright &copy; SoopsArt</small>
+          <div><small>Created by Blue Steel Web Design</small></div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer;
