e6f150bac955adb63cbc2b556425483af72acde9
benklinton <benklinton@gmail.com>
2020-11-05 12:15:09 -0700

    added footer to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../components/navbar';
+import Footer from '../components/footer';
 
 function Home() {
     return (
         <>
         <Navigation/>
+        <Footer/>
         </>
     )
 }
