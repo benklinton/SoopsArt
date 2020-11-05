c20012a531bea26f79f22f862ee483a5354f2607
benklinton <benklinton@gmail.com>
2020-11-05 11:31:31 -0700

    created home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../components/navbar';
+
+function Home() {
+    return (
+        <>
+        <Navigation/>
+        </>
+    )
+}
+
+export default Home;
