bf07b8e21a307a0a1910c4a8844984f80526b500
benklinton <benklinton@gmail.com>
2020-11-05 12:25:02 -0700

    added header to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../components/navbar';
+import Header from '../components/header';
 import Footer from '../components/footer';
 
 function Home() {
     return (
         <>
         <Navigation/>
+        <Header/>
         <Footer/>
         </>
     )
