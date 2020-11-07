3366e79e7d6dd7f8710b2ab6f04241ecfc6bd5a1
benklinton <benklinton@gmail.com>
2020-11-07 10:50:37 -0700

    created contact component

---
+++ src/components/contact/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function Contact() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Contact;
