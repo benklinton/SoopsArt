9b2b9c87487aea086d3fa3be2d386fd7bd6b2e82
benklinton <benklinton@gmail.com>
2020-11-08 15:18:27 -0700

    created sumbit email function

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-
+import axios from 'axios';
 
 
 class Contact extends React.Component {
@@ -18,64 +18,87 @@ class Contact extends React.Component {
 
     }
     onNameChange(e) {
-        this.setState({name: e.target.value})
+        this.setState({ name: e.target.value })
     }
     onEmailChange(e) {
-        this.setState({email: e.target.value})
+        this.setState({ email: e.target.value })
     }
     onSubjectChange(e) {
-        this.setState({subject: e.target.value})
+        this.setState({ subject: e.target.value })
     }
     onMsgChange(e) {
-        this.setState({message: e.target.value})
+        this.setState({ message: e.target.value })
     }
-render() {
-    return (
-        <>
-            <header className="contact-bg">
-                <Container className=" h-100">
-                    <Row className='h-100 align-items-center'>
-                        <Col lg={6} className="padding-right">
-                            <h1 className="text-format">Contact Me!</h1>
-                            <h3>
-                                If you have questions about my products, art, commissions or just wanna chat
-                                you can reach me though Instagram DMs or by filling out this form.
+
+    submitEmail(e) {
+        e.preventDefault();
+        console.log(this.state);
+        axios({
+            method: "POST",
+            url: '/send',
+            data: this.state
+        }).then((response) => {
+            if (response.data.status === 'success') {
+                alert('Message Sent');
+                this.resetForm()
+            } else if (response.data.status === 'fail') {
+                alert('Message failed, please try again')
+            }
+        })
+
+    }
+
+    resetForm() {
+        this.setState({ name: '', email: '', subject: '', message: '' })
+    }
+
+    render() {
+        return (
+            <>
+                <header className="contact-bg">
+                    <Container className=" h-100">
+                        <Row className='h-100 align-items-center'>
+                            <Col lg={6} className="padding-right">
+                                <h1 className="text-format">Contact Me!</h1>
+                                <h3>
+                                    If you have questions about my products, art, commissions or just wanna chat
+                                    you can reach me though Instagram DMs or by filling out this form.
                             </h3>
-                            <h3>Instagram: @soopsart</h3>
-                        </Col>
-                        <Col lg={6} className='padding-left'>
-                            <Form onSubmit={this.submitEmail.bind(this)} method="POST">
-                                <Form.Row>
-                                    <Col>
-                                        <Form.Control className='form-style' placeholder="Name" 
-                                        required value={this.state.name} onChange={this.onNameChange.bind(this)} />
-                                    </Col>
-                                    <Col>
-                                        <Form.Control className='form-style' placeholder="Email"
-                                        required value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-                                    </Col>
-                                </Form.Row>
-                                <Form.Row>
-                                    <Col>
-                                        <Form.Control className='mt-3 form-style' placeholder='Subject'
-                                        required value ={this.state.subject} onChange={this.onSubjectChange.bind(this)} />
-                                    </Col>
-                                </Form.Row>
-                                <Form.Row>
-                                    <Col>
-                                        <Form.Control className="mt-3 form-style" placeholder="Type your message here..." as='textarea' rows={4}
-                                        required value={this.state.message} onChange={this.onMsgChange.bind(this)}></Form.Control>
-                                    </Col>
-                                </Form.Row>
-                                <Button className='mt-3' type='submit' variant="outline-custom" block>Submit</Button>
-                            </Form>
-                        </Col>
-                    </Row>
-                </Container>
-            </header>
-        </>
-    )
+                                <h3>Instagram: @soopsart</h3>
+                            </Col>
+                            <Col lg={6} className='padding-left'>
+                                <Form onSubmit={this.submitEmail.bind(this)} method="POST">
+                                    <Form.Row>
+                                        <Col>
+                                            <Form.Control className='form-style' placeholder="Name"
+                                                required value={this.state.name} onChange={this.onNameChange.bind(this)} />
+                                        </Col>
+                                        <Col>
+                                            <Form.Control className='form-style' placeholder="Email"
+                                                required value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                                        </Col>
+                                    </Form.Row>
+                                    <Form.Row>
+                                        <Col>
+                                            <Form.Control className='mt-3 form-style' placeholder='Subject'
+                                                required value={this.state.subject} onChange={this.onSubjectChange.bind(this)} />
+                                        </Col>
+                                    </Form.Row>
+                                    <Form.Row>
+                                        <Col>
+                                            <Form.Control className="mt-3 form-style" placeholder="Type your message here..." as='textarea' rows={4}
+                                                required value={this.state.message} onChange={this.onMsgChange.bind(this)}></Form.Control>
+                                        </Col>
+                                    </Form.Row>
+                                    <Button className='mt-3' type='submit' variant="outline-custom" block>Submit</Button>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Container>
+                </header>
+            </>
+        )
     }
-    }
+}
 
 export default Contact;
